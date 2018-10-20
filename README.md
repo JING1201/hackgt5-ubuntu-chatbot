358362c17c4ce7fffc90533dda2e6eac18591d62
jing1201 <low.jing@hotmail.com>
2018-10-20 16:03:17 -0400

    Upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# hackgt5-ubuntu-chatbot
+## Description
+A chatbot trained from the Ubuntu Dialogue Corpus using tensorflow seg2seg model
+
+## Source of data analyzed
+Link: https://www.kaggle.com/rtatman/ubuntu-dialogue-corpus/home 
+
+Ryan Lowe, Nissan Pow, Iulian V. Serban and Joelle Pineau, "The Ubuntu Dialogue Corpus: A Large Dataset for Research in Unstructured Multi-Turn Dialogue Systems", SIGDial 2015. URL: http://www.sigdial.org/workshops/conference16/proceedings/pdf/SIGDIAL40.pdf
---
+++ corpora_get.py
@@ -0,0 +1,35 @@
+import pandas
+
+def read_conversations(file):
+    conversations = [] #list of lists
+    current_id = file['dialogueID'][0]
+    current_conversation = []
+    prev_user = ""
+    for index, row in file.iterrows():
+        if row['dialogueID'] != current_id:
+            if len(current_conversation) > 1: #forming a conversation
+                conversations.append(current_conversation)
+            current_conversation = []
+            prev_user = ""
+            current_id = row['dialogueID']
+        if row['from'] != prev_user:
+            current_conversation.append(str(row['text']))
+        else:
+            current_conversation[len(current_conversation)-1] += str(row['text']) #one person talking continuously
+        prev_user = row['from']
+    return conversations
+
+def get_tokenized_sequencial_sentences(conversations):
+   for conversation in conversations:
+       for i in range(len(conversation) - 1):
+           yield (conversation[i].split(" "), conversation[i+1].split(" "))
+    
+def generate_conv_tuple(file):
+    conversations = read_conversations(file)
+    return tuple(zip(*list(get_tokenized_sequencial_sentences(conversations))))
+
+def get_ubuntu_corpus_data():
+    file = pandas.read_csv('Ubuntu-dialogue-corpus/dialogueText.csv')
+    #ile.append(pandas.read_csv('Ubuntu-dialogue-corpus/dialogueText_301.csv'))
+    #file.append(pandas.read_csv('Ubuntu-dialogue-corpus/dialogueText_196.csv'))
+    return generate_conv_tuple(file)
